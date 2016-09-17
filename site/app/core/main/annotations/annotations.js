81eda20839766042c9a4f031a3bd589ec17950e7
Sabino Velasquez <sabinovelasquez@gmail.com>
2016-09-16 22:30:47 -0300

    first date approach

--- site/app/core/main/annotations/annotations.js
+++ site/app/core/main/annotations/annotations.js
@@ -1,13 +1,17 @@
 export default ngModule => {
   require('./annotations.scss');
-
+  const moment = require('moment');
   ngModule.directive('annotations', function annotations(fbAPIService) {
     return {
       template: require('./annotations.jade'),
       scope: {},
       controllerAs: 'ann',
       controller: function annotationsCtrl() {
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = date.getMonth();
         this.students = fbAPIService.getClass('1A');
+        this.month = moment().startOf('month').weekday(1).format('MMMM Do YYYY');
       },
     };
   });
