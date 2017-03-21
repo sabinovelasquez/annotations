82964b3cafc63cadf279b57ac3fba0b4b9100a6a
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-03-21 16:29:55 -0300

    fix de ruteo

--- site/app/core/services.js
+++ site/app/core/services.js
@@ -1,4 +1,4 @@
 export default ngModule => {
-  require('./main/services/fbservice')(ngModule);
-  require('./main/services/calAPIService')(ngModule);
+  require('./services/fbservice')(ngModule);
+  require('./services/calAPIService')(ngModule);
 };
