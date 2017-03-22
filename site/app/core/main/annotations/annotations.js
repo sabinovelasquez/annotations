bdce33c6a22a0ed5ad2a3a43d0a56bd1cd77db90
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-03-22 17:14:53 -0300

    smart table directive + dummy

--- site/app/core/main/annotations/annotations.js
+++ site/app/core/main/annotations/annotations.js
@@ -10,6 +10,30 @@ export default ngModule => {
           this.students = data;
         });
         this.weeks = [calAPIService.firstWeek, calAPIService.secondWeek, calAPIService.thirdWeek, calAPIService.forthWeek];
+        this.predicates = ['firstName', 'lastName', 'birthDate', 'balance', 'email'];
+        this.selectedPredicate = this.predicates[0];
+        this.rowCollection = [
+          {
+            firstName: 'Laurent',
+            lastName: 'Renard',
+            birthDate: new Date('1987-05-21'),
+            balance: 102,
+            email: 'whatever@gmail.com',
+          },
+          {
+            firstName: 'Blandine',
+            lastName: 'Faivre',
+            birthDate: new Date('1987-04-25'),
+            balance: -2323.22,
+            email: 'oufblandou@gmail.com'},
+          {
+            firstName: 'Francoise',
+            lastName: 'Frere',
+            birthDate: new Date('1955-08-27'),
+            balance: 42343,
+            email: 'raymondef@gmail.com',
+          },
+        ];
       },
     };
   });
