434014f468123e9599c80e4610520c91eac9f02c
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-03-24 21:25:42 -0300

    table with service

--- site/app/core/main/annotations/annotations.js
+++ site/app/core/main/annotations/annotations.js
@@ -6,9 +6,12 @@ export default ngModule => {
       scope: {},
       controllerAs: 'ann',
       controller: function annotationsCtrl() {
-        fbAPIService.getClass('1A').$loaded().then( (data) => {
-          this.students = data;
-        });
+        this.callServer = () => {
+          this.isLoading = true;
+          fbAPIService.getClass('1A').$loaded().then( (data) => {
+            this.students = data;
+          });
+        };
         this.predicates = ['number', 'name', 'lastname'];
         this.selectedPredicate = this.predicates[0];
         this.weeks = [calAPIService.firstWeek, calAPIService.secondWeek, calAPIService.thirdWeek, calAPIService.forthWeek];
