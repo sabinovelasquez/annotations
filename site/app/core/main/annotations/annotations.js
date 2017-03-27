a470b20762f61773679d10a5bae4faa1144c2da8
Sabino Velasquez <sabinovelasquez@gmail.com>
2017-03-27 20:23:41 -0300

    edit-safe mode

--- site/app/core/main/annotations/annotations.js
+++ site/app/core/main/annotations/annotations.js
@@ -65,9 +65,9 @@ export default ngModule => {
           }
           this.updateVals();
         };
-        this.sendWarning = (week) => {
-          console.log(week);
-        };
+        // this.sendWarning = (week) => {
+        //   console.log(week);
+        // };
         this.weeks = [calAPIService.firstWeek, calAPIService.secondWeek, calAPIService.thirdWeek, calAPIService.forthWeek];
       },
     };
